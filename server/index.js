9be062ccccc441cdaf837b8253552716a4bd856c
ShiunMurata <purpleclouds0916@gmail.com>
2022-03-21 11:36:57 +0900

    サーバーの追加

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+
+//ミドルウエアでstaticパスを追加（ただ、これだけだと直アクセスや無いpathだと動かない）
+app.use(express.static(path.join(__dirname, "..", "build")));
+
+//これを追加（全てをindex.htmlにリダイレクト。いわゆるrewrite設定）
+app.use((req, res, next) => {
+    res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+});
+
+app.listen(3006, () => {
+    console.log("server started on port 3006");
+});
